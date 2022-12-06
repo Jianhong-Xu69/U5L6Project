1cdf4f3d4681f1c501ed7ed0045ab8fc447c4b4b
BT_4N2_14 <jxu5406@bths.edu>
2022-12-06 12:25:55 -0500

    Patchnotes v1.02
    Some more work, oh god this will be painful

--- src/BankApp.java
+++ src/BankApp.java
@@ -1,13 +1,13 @@
 public class BankApp {
-
     private Bank robinDaBank;
     private BagelShop leOldeShoppe;
     private CreditCard howDoTheseWork;
+    private CreditCard whyDoYouWantASecondOne;
 
-    public BankApp (String name) {
+    public BankApp () {
         robinDaBank = new Bank();
         leOldeShoppe = new BagelShop("Le Olde Shoppe", 500, 2, robinDaBank);
-        howDoTheseWork = new CreditCard(name, String.valueOf((int) (Math.random()*9000)+1000));
+        howDoTheseWork = new CreditCard("default", String.valueOf((int) (Math.random()*9000)+1000));
     }
 
     public String buyThings (CreditCard card, int quantity, String cardPIN) {
@@ -18,5 +18,21 @@ public class BankApp {
         }
     }
 
+    public String payDebt (CreditCard card, int amount) {
+        howDoTheseWork.reduceBalance(amount);
+        return "You now owe $" + (howDoTheseWork.getBalanceOwed());
+    }
 
+    public CreditCard newOne(String name) {
+        whyDoYouWantASecondOne = new CreditCard(name, String.valueOf((int) (Math.random()*9000)+1000));
+        return whyDoYouWantASecondOne;
+    }
+
+    public String CardCompare() {
+        if (robinDaBank.lowerBalance(howDoTheseWork,whyDoYouWantASecondOne) != null) {
+            return robinDaBank.lowerBalance(howDoTheseWork,whyDoYouWantASecondOne) + "\nHas less debt";
+        } else {
+            return howDoTheseWork + "\n" + whyDoYouWantASecondOne + "\nThey are the same";
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //BankApp app = new BankApp();
+        BankApp app = new BankApp();
         // call methods from your BankApp object to handle program logic
-        for (int i = 0; i < 10000; i++) {
-            System.out.println((int) (Math.random()*9000)+999);
-        }
+
     }
 }
